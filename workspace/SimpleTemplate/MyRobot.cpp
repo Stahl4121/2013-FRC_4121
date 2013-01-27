528417fe5815652ccc92f8949fccc43e09735adf
Jeffrey Armstrong <Armstrong.Jeffrey@gmail.com>
2013-01-27 11:13:07 -0500

    Initial Commit
    First Commit - 1/27/2013
    Autonomous mode - Robot will drive foward on a timer
    
    Tele-Op - Robot can be toggled between Arcade and Tank drive styles with
    a button press, Pulling the trigger on the right stick (I think)
    activates the "shooter" test motor, button 8 and 9 decrement/increment
    the turn sensitivity.

---
+++ workspace/SimpleTemplate/MyRobot.cpp
@@ -0,0 +1,181 @@
+#include "WPILib.h"
+#include <PWM.h>
+//#include <Jaguar.h>
+/**
+ * This is a demo program showing the use of the RobotBase class.
+ * The SimpleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ */ 
+
+class RobotDemo : public SimpleRobot
+{
+	RobotDrive myRobot; // robot drive system
+	Joystick left_jstick; // only joystick
+	Joystick right_jstick;
+
+	Jaguar motor_shooter;
+
+	static const float m_sensitivity = .25;
+	static const int NUM_JOYSTICK_BUTTONS = 16;
+	bool m_rightStickButtonState[(NUM_JOYSTICK_BUTTONS+1)];
+	bool m_leftStickButtonState[(NUM_JOYSTICK_BUTTONS+1)];	
+
+
+public:
+
+	RobotDemo(void):
+		myRobot(1, 2),	// these must be initialized in the same order
+		left_jstick(1),		// as they are declared above.
+		right_jstick(2),
+		motor_shooter(3)
+	{
+		myRobot.SetExpiration(0.1);
+	}
+
+	/**
+	 * Drive left & right motors for 2 seconds then stop
+	 */
+
+	void Autonomous(void)
+	{
+		cout<<"Here";
+		myRobot.SetSafetyEnabled(false);
+		myRobot.Drive(.2, 0.0); 	// drive forwards half speed
+		Wait(.2); 				//    for 2- seconds
+		myRobot.Drive(0.0, 0.0); 	// stop robot
+		Wait(.5);
+		myRobot.Drive(.2, .5);
+		Wait(.2);
+
+		printf("here 2");
+		myRobot.Drive(.2, 0.0); 	// drive forwards half speed
+		Wait(.2); 				//    for 2- seconds
+		myRobot.Drive(0.0, 0.0); 	// stop robot
+		Wait(.5);
+		myRobot.Drive(.2, .5);
+		Wait(.2);
+
+		myRobot.Drive(0.0, 0.0);
+
+	}
+
+	/**
+	 * Runs the motors with arcade steering. 
+	 */
+	void OperatorControl(void)
+	{
+		float sensitivity = -.9;
+		bool drive_style = true;
+
+		myRobot.SetSafetyEnabled(false);
+
+		DriverStationLCD *ds = DriverStationLCD::GetInstance();
+		myRobot.SetExpiration(5);
+		float val = myRobot.GetExpiration();
+
+		ds->PrintfLine(DriverStationLCD::kUser_Line1, "Expiration Value: %d", val);
+		ds->UpdateLCD();
+
+		myRobot.SetInvertedMotor(myRobot.kRearLeftMotor,true);
+		myRobot.SetInvertedMotor(myRobot.kRearRightMotor,true);
+		static int count_drive = 0;
+		
+		while (IsOperatorControl())
+		{
+			if (count_drive > 10000){
+
+				if (right_jstick.GetTrigger() == drive_style)
+				{
+					drive_style=false;
+					myRobot.TankDrive(left_jstick, right_jstick);
+					Wait(0.001);// wait for a motor update time
+					
+				}
+				else 
+				{
+					drive_style=true;
+					myRobot.ArcadeDrive(right_jstick);
+					Wait(0.001);				// wait for a motor update time
+				}
+				
+				/*
+				 * if (right_jstick.GetTrigger() == drive_style)
+				{
+					drive_style=false;			
+					myRobot.TankDrive(left_jstick, right_jstick);
+					Wait(0.001);				// wait for a motor update time
+				}
+				else 
+				{
+					drive_style=true;
+					myRobot.ArcadeDrive(right_jstick);
+					Wait(0.001);				// wait for a motor update time
+				}
+				
+				 */
+				count_drive=0;
+			}
+			count_drive++;
+
+			if(right_jstick.GetRawButton(2))
+			{
+				motor_shooter.Set(.5);
+			}
+			else
+			{
+				motor_shooter.Disable();
+			}
+
+
+			static int count = 0;
+			if (count > 5000)
+			{
+				if(right_jstick.GetRawButton(8))
+				{
+					sensitivity-=.05;
+					if(sensitivity > 10)
+					{
+						sensitivity = 10;
+					}
+
+					myRobot.SetSensitivity(sensitivity);
+					ds->PrintfLine(DriverStationLCD::kUser_Line4, "Sensitivity = %f", sensitivity);
+				}
+
+				if(right_jstick.GetRawButton(9))
+				{
+					sensitivity+=.05;
+					if(sensitivity > 10)
+					{
+						sensitivity = 10;
+					}
+					myRobot.SetSensitivity(sensitivity);
+					ds->PrintfLine(DriverStationLCD::kUser_Line4, "Sensitivity = %f", sensitivity);
+				}
+				count = 0;
+			}
+			count++;
+
+
+
+			ds->UpdateLCD();
+		}
+	}
+
+	/**
+	 * Runs during test mode
+	 */
+	void Test() {
+		myRobot.SetSafetyEnabled(true);
+		while (IsTest())
+		{
+			myRobot.TankDrive(left_jstick, right_jstick); // drive with arcade style (use right stick)
+			Wait(0.005);				// wait for a motor update time
+		}
+
+	}
+};
+
+START_ROBOT_CLASS(RobotDemo);
+
