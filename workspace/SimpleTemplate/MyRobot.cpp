a09ad8c982955a92d3ca75421f49d6b15888e623
Jeffrey S. Armstrong <Armstrong.Jeffrey@gmail.com>
2013-01-31 20:11:56 -0500

    Added much functionality
    Added Battery Low Level, Gyro , Accelerometer and displayed to Driver
    Station LCD.   Accerometer is using the wrong class, see the issue for
    more details.

--- workspace/SimpleTemplate/MyRobot.cpp
+++ workspace/SimpleTemplate/MyRobot.cpp
@@ -1,12 +1,8 @@
 #include "WPILib.h"
 #include <PWM.h>
+#include "Accelerometer.h"// should be using I2C interface, NOT ANALOG!!!!!!
 //#include <Jaguar.h>
-/**
- * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- */ 
+
 
 class RobotDemo : public SimpleRobot
 {
@@ -17,6 +13,8 @@ class RobotDemo : public SimpleRobot
 	Timer drive_timer;
 	Timer timer;
 	Timer trigger_timer;
+	Gyro gyro;
+	Accelerometer accel;
 	static const float m_sensitivity = .25;
 	static const int NUM_JOYSTICK_BUTTONS = 16;
 	bool m_rightStickButtonState[(NUM_JOYSTICK_BUTTONS+1)];
@@ -32,7 +30,9 @@ public:
 		motor_shooter(3),
 		drive_timer(),
 		timer(),
-		trigger_timer()
+		trigger_timer(),
+		gyro(1),
+		accel(2)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -69,119 +69,122 @@ public:
 	 */
 	void OperatorControl(void)
 	{
-		float sensitivity = -.9;
+		float voltage;
+		float sensitivity = .25;
 		bool drive_style = true;
-
+		gyro.Reset();
+		float angle = gyro.GetAngle();  // current heading (0 = target)
+		float acceleration = accel.GetAcceleration();
 		myRobot.SetSafetyEnabled(false);
 
+		//char name = 'body';
+		//char *sysName;
+		//sysName = &name;
+
+		//Subsystem body = Subsystem(sysName);
+
 		DriverStationLCD *ds = DriverStationLCD::GetInstance();
-		myRobot.SetExpiration(5);
+		DriverStation *station = DriverStation::GetInstance();
+
+		//LiveWindow *lw = LiveWindow::GetInstance();
+		//lw->AddSensor('body','Accelerometer',accel);
+		//lw->AddSensor()
+		//myRobot.SetExpiration(5);
+
 		float val = myRobot.GetExpiration();
 
 		ds->PrintfLine(DriverStationLCD::kUser_Line1, "Expiration Value: %d", val);
+		ds->PrintfLine(DriverStationLCD::kUser_Line5, "Gyro Value: %d", angle);
+		ds->PrintfLine(DriverStationLCD::kUser_Line6, "Acceleration: %d", acceleration);
 		ds->UpdateLCD();
 
 		myRobot.SetInvertedMotor(myRobot.kRearLeftMotor,true);
 		myRobot.SetInvertedMotor(myRobot.kRearRightMotor,true);
 		drive_timer.Start();
 		timer.Start();
-		
+
 		bool current_trigger;
 		bool previous_trigger;
 		bool stable_trigger;
-		bool thirdState;
-		
+
 		unsigned int trigger_state = 0;
-//		unsigned int trigger_count = 0;
+		//		unsigned int trigger_count = 0;
 		const double trigger_bounce_time_S = .1;  //this is in seconds//#fix change to a #define 
-		
+
 		while(IsOperatorControl())
 		{
 			previous_trigger = current_trigger;
 			current_trigger = right_jstick.GetTrigger();
-			
+
 			switch (trigger_state)
 			{
-				case 0:  //No Button Press 
-					//current_trigger = right_jstick.GetTrigger();					
-					if (current_trigger)
-					{
-						trigger_timer.Reset();
-						trigger_state = 1;
-					}
-					else
-					{
-						trigger_state = 0;
-					}
-					break;
-				case 1:
-					trigger_timer.Start();
-					trigger_state = 2;
-					break;
-				case 2:
-					if (trigger_timer.Get() > trigger_bounce_time_S)
-					{
-						trigger_timer.Stop();
-						trigger_timer.Reset();
-						trigger_state = 3;
-					}
-					else
-					{
-						trigger_state = 2;
-					}
-					
-					break;
-				case 3:
-					
-					stable_trigger = current_trigger;
-					if (previous_trigger != current_trigger)
-					{
-						trigger_state = 0;
-					}
-					else
-					{
-						
-						previous_trigger = current_trigger;
-					}
-					break;
-				default:
+			case 0:  //No Button Press 
+				//current_trigger = right_jstick.GetTrigger();					
+				if (current_trigger)
+				{
+					trigger_timer.Reset();
+					trigger_state = 1;
+				}
+				else
+				{
 					trigger_state = 0;
-					break;
+				}
+				break;
+			case 1:
+				trigger_timer.Start();
+				trigger_state = 2;
+				break;
+			case 2:
+				if (trigger_timer.Get() > trigger_bounce_time_S)
+				{
+					trigger_timer.Stop();
+					trigger_timer.Reset();
+					trigger_state = 3;
+				}
+				else
+				{
+					trigger_state = 2;
+				}
+
+				break;
+			case 3:
+
+				stable_trigger = current_trigger;
+				if (previous_trigger != current_trigger)
+				{
+					trigger_state = 0;
+				}
+				else
+				{
+
+					previous_trigger = current_trigger;
+				}
+				break;
+			default:
+				trigger_state = 0;
+				break;
 			}
 			ds->PrintfLine(DriverStationLCD::kUser_Line1, "trigger_state", trigger_state);			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 			//if (drive_timer.Get() > .1){
 
 
-				if (stable_trigger == drive_style)//(right_jstick.GetTrigger() == drive_style)
-				{
-					drive_style=false;
-					myRobot.TankDrive(left_jstick, right_jstick);
-					Wait(0.001);// wait for a motor update time
-					
-				}
-				else 
-				{
-					drive_style=true;
-					myRobot.ArcadeDrive(right_jstick);
-					Wait(0.001);				// wait for a motor update time
-				}
-				
-				/*
-				 * if (right_jstick.GetTrigger() == drive_style)
+			if (stable_trigger == drive_style)//(right_jstick.GetTrigger() == drive_style)
+			{
+				drive_style=false;
+				myRobot.TankDrive(left_jstick, right_jstick);
+				Wait(0.001);// wait for a motor update time
+
+			}
+			else 
+			{
+				drive_style=true;
+				myRobot.ArcadeDrive(right_jstick);
+				Wait(0.001);				// wait for a motor update time
+			}
+
+			/*
+			 * if (right_jstick.GetTrigger() == drive_style)
 				{
 					drive_style=false;			
 					myRobot.TankDrive(left_jstick, right_jstick);
@@ -193,9 +196,9 @@ public:
 					myRobot.ArcadeDrive(right_jstick);
 					Wait(0.001);				// wait for a motor update time
 				}
-				
-				 */
-				drive_timer.Reset();
+
+			 */
+			drive_timer.Reset();
 			//}
 
 			if(right_jstick.GetRawButton(2))
@@ -208,7 +211,7 @@ public:
 			}
 
 
-			
+
 			if (timer.Get() > .005)
 			{
 
@@ -236,8 +239,26 @@ public:
 				}
 				timer.Reset();
 			}
+			//ds->PrintfLine(DriverStationLCD::kUser_Line1, "trigger_state", trigger_state);	
+			angle= gyro.GetAngle();
+			acceleration= accel.GetAcceleration();
+			ds->PrintfLine(DriverStationLCD::kUser_Line5, "Gyro Value: %d", angle);
+			ds->PrintfLine(DriverStationLCD::kUser_Line6, "Acceleration: %d", acceleration);
+			voltage = station->GetBatteryVoltage();
+			if(voltage<10){
+				ds->PrintfLine(DriverStationLCD::kUser_Line3, "Battery is low..");
+			}
+			else
+			{
+				ds->PrintfLine(DriverStationLCD::kUser_Line3, "Battery is fine");
+			}
+			
 			ds->UpdateLCD();
 		}
+
+
+
+
 	}
 
 	/**
